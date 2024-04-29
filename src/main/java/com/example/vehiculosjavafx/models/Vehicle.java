498635e75d20239e2b3721799d15ca7222bd2932
david <dperezfernandez@safareyes.es>
2024-04-29 11:30:48 +0200

    ✨ New vehicle model

---
+++ src/main/java/com/example/vehiculosjavafx/models/Vehicle.java
@@ -0,0 +1,12 @@
+package com.example.vehiculosjavafx.models;
+
+public class Vehicle {
+    private int id;
+    private String description;
+    private String brand;
+    private String tuition; // Matrícula
+    private double kmDone;
+    private double pricePerDay;
+    private boolean isRented;
+
+}
