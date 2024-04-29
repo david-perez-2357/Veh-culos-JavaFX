156629738fdb31b3530db02bb7b770cdba77db12
david <dperezfernandez@safareyes.es>
2024-04-29 11:30:57 +0200

    ✨ New vehicle rent model

---
+++ src/main/java/com/example/vehiculosjavafx/models/VehicleRent.java
@@ -0,0 +1,11 @@
+package com.example.vehiculosjavafx.models;
+
+import java.time.LocalDate;
+
+public class VehicleRent {
+    private Vehicle vehicle;
+    private Customer customer;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private double totalPrice;
+}
