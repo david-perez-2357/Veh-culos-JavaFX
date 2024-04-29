1afe03b456afc104a11799334a0ee843ba88d8a0
david <dperezfernandez@safareyes.es>
2024-04-29 11:30:36 +0200

    ✨ New customer model

---
+++ src/main/java/com/example/vehiculosjavafx/models/Customer.java
@@ -0,0 +1,10 @@
+package com.example.vehiculosjavafx.models;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String surname;
+    private String nif;
+    private String address;
+    private String city;
+}
