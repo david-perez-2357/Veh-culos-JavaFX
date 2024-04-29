2ad0f1c81fcd924cf9528e8eba3b3120f7cf9d8d
david <dperezfernandez@safareyes.es>
2024-04-29 14:41:58 +0200

    ♻️ Change function to another file

--- src/main/java/com/example/vehiculosjavafx/AddVehicleApp.java
+++ src/main/java/com/example/vehiculosjavafx/AddVehicleApp.java
@@ -1,9 +1,7 @@
 package com.example.vehiculosjavafx;
 
-import com.example.vehiculosjavafx.models.Customer;
 import com.example.vehiculosjavafx.models.Vehicle;
-import com.example.vehiculosjavafx.models.VehicleRent;
-import com.example.vehiculosjavafx.utils.Database;
+import com.example.vehiculosjavafx.GlobalVariables.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,22 +10,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
+
+import static com.example.vehiculosjavafx.GlobalVariables.*;
 
 public class AddVehicleApp extends Application {
-    private List<Vehicle> vehicles = new ArrayList<>();
-    private List<Customer> customers = new ArrayList<>();
-    private List<VehicleRent> rents = new ArrayList<>();
-    public static final Database db;
 
-    static {
-        try {
-            db = new Database("jdbc:mysql://localhost:3306/rentavehiculos", "root", "");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @Override
     public void start(Stage stage) throws IOException, SQLException {
@@ -40,6 +28,10 @@ public class AddVehicleApp extends Application {
         AddVehicleController controller = fxmlLoader.getController();
 
         getVehicles();
+        getCustomers();
+        getRents();
+        getActiveRents();
+
         System.out.println(vehicles);
     }
 
@@ -47,24 +39,4 @@ public class AddVehicleApp extends Application {
         launch();
     }
 
-    public void getVehicles() throws SQLException {
-        ResultSet result = db.executeQuery("SELECT vh.*, IF(sv.fecha_alquiler <= CURRENT_DATE() AND sv.fecha_entrega > CURRENT_DATE(), 1, 0) AS esta_alquilado " +
-                "FROM vehiculos vh " +
-                "LEFT JOIN servicios sv ON vh.matricula = sv.matricula_vehiculo " +
-                "GROUP BY vh.matricula;"
-        );
-
-        while (result.next()) {
-            vehicles.add(new Vehicle(
-                    result.getString("descripcion"),
-                    result.getString("marca"),
-                    result.getString("matricula"),
-                    result.getInt("kilometros"),
-                    result.getInt("precio"),
-                    result.getBoolean("esta_alquilado")
-            ));
-        }
-
-        result.close();
-    }
 }
