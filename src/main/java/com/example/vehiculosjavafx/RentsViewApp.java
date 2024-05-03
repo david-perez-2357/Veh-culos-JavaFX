caa4c38a58f027136b48aa3789c394ee392a243d
david <dperezfernandez@safareyes.es>
2024-05-03 11:45:32 +0200

    ✨ New controller and App

--- src/main/java/com/example/vehiculosjavafx/RentsViewApp.java
+++ src/main/java/com/example/vehiculosjavafx/RentsViewApp.java
@@ -1,4 +1,25 @@
 package com.example.vehiculosjavafx;
 
-public class RentsViewApp {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class RentsViewApp extends Application {
+
+    @FXML
+    public void start(Stage stage) throws IOException, SQLException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RentsViewApp.class.getResource("rents_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 500);
+        stage.setTitle("Alquileres");
+        stage.setScene(scene);
+        stage.show();
+
+        RentsViewController controller = fxmlLoader.getController();
+    }
+
 }
--- src/main/java/com/example/vehiculosjavafx/RentsViewController.java
+++ src/main/java/com/example/vehiculosjavafx/RentsViewController.java
@@ -1,4 +1,99 @@
 package com.example.vehiculosjavafx;
 
+import com.example.vehiculosjavafx.models.VehicleRent;
+import com.example.vehiculosjavafx.utils.TableViewManager;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static com.example.vehiculosjavafx.GlobalVariables.*;
+import static com.example.vehiculosjavafx.GlobalVariables.getActiveRents;
+
 public class RentsViewController {
+    @FXML
+    private DatePicker startDate;
+    @FXML
+    private DatePicker endDate;
+    @FXML
+    private ComboBox customerSelector;
+    @FXML
+    private TableView rentTable;
+    private TableViewManager<VehicleRent> rentTableManager;
+    @FXML
+    private Button newRentButton;
+
+    public void initialize() throws SQLException {
+        rentTableManager = new TableViewManager<VehicleRent>(rentTable);
+        rentTableManager.addColumn("Matrícula", r -> r.getVehicle().getTuition());
+        rentTableManager.addColumn("Marca", r -> r.getVehicle().getBrand());
+        rentTableManager.addColumn("Fecha alquiler", VehicleRent::getStartDate);
+        rentTableManager.addColumn("Fecha entrega", VehicleRent::getEndDate);
+        rentTableManager.addColumn("Precio total", VehicleRent::getTotalPrice);
+
+        getVehicles();
+        getCustomers();
+        getRents();
+        getActiveRents();
+
+        rentTableManager.addAllData(rents);
+
+        customerSelector.getItems().addAll(customers);
+    }
+
+    private void clearFilters(String exception) {
+        if (!exception.equals("startDate")) {
+            startDate.setValue(null);
+        }
+
+        if (!exception.equals("endDate")) {
+            endDate.setValue(null);
+        }
+
+        if (!exception.equals("customer")) {
+            customerSelector.setValue(null);
+        }
+    }
+
+    @FXML
+    private void startDateChanged() {
+        if (startDate.getValue() == null) {
+            return;
+        }
+
+        clearFilters("startDate");
+        rentTableManager.filter(rent -> !rent.getStartDate().isBefore(startDate.getValue()));
+    }
+
+    @FXML
+    private void endDateChanged() {
+        if (endDate.getValue() == null) {
+            return;
+        }
+
+        clearFilters("endDate");
+        rentTableManager.filter(rent -> !rent.getEndDate().isAfter(endDate.getValue()));
+    }
+
+    @FXML
+    private void customerSelectorChanged() {
+        if (customerSelector.getValue() == null) {
+            return;
+        }
+
+        clearFilters("customer");
+        rentTableManager.filter(rent -> rent.getCustomer().equals(customerSelector.getValue()));
+    }
+
+    @FXML
+    private void newRentClick() throws SQLException, IOException {
+        newRentButton.getScene().getWindow().hide();
+        new AddVehicleApp().start((Stage) newRentButton.getScene().getWindow());
+    }
 }
